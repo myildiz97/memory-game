d8aadcda204705c966d4c2aed8c1c38e18e08252
Mehmet Yıldız <yildiz.mehmet@metu.edu.tr>
2024-04-08 23:32:36 +0300

    Remove resetSettings call in StartGame component

--- memory-game/src/components/StartGame.tsx
+++ memory-game/src/components/StartGame.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { Button } from './ui/Button';
 import { useNavigate } from 'react-router-dom';
-import { resetSettings } from '@/redux/settings/settingsSlice';
 
 interface IStartGameProps {
 }
@@ -10,8 +9,6 @@ const StartGame: FC<IStartGameProps> = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    // If the user starts the game, reset the settings
-    resetSettings();
     navigate('/game');
   }
 
